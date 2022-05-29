18cc94a45608a8f4b019fd80f22f070b6fe91187
ansk1d <105351282+ansk1d@users.noreply.github.com>
2022-05-29 16:32:57 +0300

    its done!

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function iflogedin() {
+    document.getElementById("loginbtn").innerText = "logout";
+}
+
+function hide(element) {
+    element.remove();
+}
+
+function likeBox(){
+    alert("ninja was liked!");
+}
+
